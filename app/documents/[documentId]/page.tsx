1f10b1446c602421e28c28163690040784ea7a19
Bibek K C <bibekyess@gmail.com>
2025-07-18 18:35:31 +0900

    Adds navbar tools

--- app/documents/[documentId]/page.tsx
+++ app/documents/[documentId]/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import { Editor } from './editor';
 import { Toolbar } from './toolbar';
+import NavBar from './navbar';
 
 interface DocumentIdPageProps {
   params: Promise<{ documentId: string }>
@@ -12,8 +13,15 @@ const DocumentIdPage = async ({ params }: DocumentIdPageProps) => {
   return (
     <div className='min-h-screen bg-[#FAFBFD]'>
       {/* Here I cannot pass the editor as prop to toolbar, so we are using zustand to create the editor store */}
-      <Toolbar />
-      <Editor />
+      <div className='flex flex-col px-4 pt-2 gap-y-2 fixed top-0 left-0 right-0 z-10 bg-[#FAFBFD] print:hidden'>
+        <NavBar />
+        <Toolbar />
+      </div>
+      
+      {/* A trick to get this 114px number is to hover by using the dev terminal.. and then it will show the pixel width and heignt of the hovered component */}
+      <div className='pt-[114px] print:pt-0'>
+        <Editor />
+      </div>
     </div>
   )
 }
